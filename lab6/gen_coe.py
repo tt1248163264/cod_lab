7b5f5dd8932ef9fc704717cfa64922e5c74c783a
tt1248163264 <1982784298@qq.com>
2019-05-29 12:39:43 +0800

    gen_coe

--- lab6/gen_coe.py
+++ lab6/gen_coe.py
@@ -1,7 +1,7 @@
-with open('cod_lab6_bg.coe','w') as f:
-    f.writelines('memory_initialization_radix = 16;\n')
-    f.writelines('memory_initialization_vector = \n')
+with open('temp.coe','w') as f:
+    #f.writelines('memory_initialization_radix = 16;\n')
+    #f.writelines('memory_initialization_vector = \n')
     #for i in range(499):
     #    f.writelines('00000000\n')
-    for i in range(65536):
-        f.writelines('fff\n')
+    for i in range(256):
+        f.writelines('00000000\n')
